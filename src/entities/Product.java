8c167e4ffc876a99b092ee26df92c63f5a295de1
mpaulofg <mpaulofg@gmail.com>
2022-06-24 08:58:00 -0300

    Static method

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -21,6 +21,10 @@ public class Product {
 		this.price = price;
 	}
 	
+	public static void staticPriceUpdate(Product p) {
+		p.setPrice(p.getPrice() *1.1);
+	}
+	
 	public String toString() {
 		return name + ", "+ String.format("%.2f", price);
 	}
